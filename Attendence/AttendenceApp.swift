b6a6474a35647f3d01c7fa941636b83bb26476a9
アウン <hlumaungphyo@gmail.com>
2024-11-12 17:08:38 +0900

    Initial Commit

---
+++ Attendence/AttendenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendenceApp.swift
+//  Attendence
+//
+//  Created by Hlwan Aung Phyo on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AttendenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
